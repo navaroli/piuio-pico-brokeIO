00397a98c5448725393bf94b51c6febf39f6176f
navaroli <nick.navaroli@gmail.com>
2026-07-31 22:56:03 -0700

    Support for ITGMania lights while in gamepad mode

--- main.c
+++ main.c
@@ -687,6 +687,8 @@ void tud_hid_set_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_
             lxio_set_report(buffer, bufsize, &lights);
         } else if (input_mode == INPUT_MODE_GAMECUBE) {
             // rumble
+        } else if (input_mode == INPUT_MODE_GAMEPAD) {
+            hid_set_report(buffer, bufsize, &lights);
         }
     }
 }
